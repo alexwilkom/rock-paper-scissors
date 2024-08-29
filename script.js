bec2a2d17311348a57d32bb8c19c767342d18fe9
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-29 12:57:58 +0200

    Create a function for the computer choice

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber]
+}
