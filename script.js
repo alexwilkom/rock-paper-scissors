bfab33f30d66fc5c8070975140ee340b8d12ef6a
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-29 14:16:22 +0200

    Create playGame function to determine the winner

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const choices = ["rock", "paper", "scissors"];
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber]
@@ -14,7 +11,7 @@ function getHumanChoice() {
 
         if (choice === null) {
             alert("Game cancelled.")
-            return
+            return "";
         }
 
         const choiceFormatted = choice.toLowerCase();
@@ -27,19 +24,49 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    console.log(`The computer chose ${computerChoice}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let rounds = 5;
 
-    const outcomes = {
-        rock: { rock: 'It is a tie!', paper: 'You lose! Paper beats Rock.', scissors: 'You win! Rock beats Scissors.' },
-        paper: { rock: 'You win! Paper beats Rock.', paper: 'It is a tie!', scissors: 'You lose! Scissors beat Paper.' },
-        scissors: { rock: 'You lose! Rock beats Scissors.', paper: 'You win! Scissors beat Paper.', scissors: 'It is a tie!' }
-    };
+    console.log('The winner in 5 rounds.');
 
-    const outcome = outcomes[humanChoice][computerChoice];
 
-    if (outcome.includes('win')) humanScore += 1;
-    if (outcome.includes('lose')) computerScore += 1;
+    function playRound(humanChoice, computerChoice) {
+        console.log(`The computer chose ${computerChoice}.`);
+        console.log(`You chose ${humanChoice}.`);
 
-    console.log(outcome);
+        const outcomes = {
+            rock: { rock: 'It is a tie!', paper: 'You lose! Paper beats Rock.', scissors: 'You win! Rock beats Scissors.' },
+            paper: { rock: 'You win! Paper beats Rock.', paper: 'It is a tie!', scissors: 'You lose! Scissors beat Paper.' },
+            scissors: { rock: 'You lose! Rock beats Scissors.', paper: 'You win! Scissors beat Paper.', scissors: 'It is a tie!' }
+        };
+
+        const outcome = outcomes[humanChoice][computerChoice];
+
+        if (outcome.includes('win')) humanScore += 1;
+        if (outcome.includes('lose')) computerScore += 1;
+
+        console.log(outcome);
+    }
+
+    for (let i = rounds; i > 0; i--) {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Rounds left: ${i - 1}`);
+    }
+
+    if (humanScore > computerScore) {
+        console.log('You have won the game!');
+    } else if (humanScore < computerScore) {
+        console.log('You have lost the game!');
+    } else {
+        console.log('No one wins! But no one loses either!');
+    }
+
 }
+
+playGame();
