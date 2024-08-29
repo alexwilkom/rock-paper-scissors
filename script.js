0abef33808b161e2ad08a98c2c5a3836169e2516
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-29 13:48:41 +0200

    Create the function to play a single round

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const choices = ["rock", "paper", "scissors"];
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -26,4 +27,19 @@ function getHumanChoice() {
     }
 }
 
-getHumanChoice()
+function playRound(humanChoice, computerChoice) {
+    console.log(`The computer chose ${computerChoice}`);
+
+    const outcomes = {
+        rock: { rock: 'It is a tie!', paper: 'You lose! Paper beats Rock.', scissors: 'You win! Rock beats Scissors.' },
+        paper: { rock: 'You win! Paper beats Rock.', paper: 'It is a tie!', scissors: 'You lose! Scissors beat Paper.' },
+        scissors: { rock: 'You lose! Rock beats Scissors.', paper: 'You win! Scissors beat Paper.', scissors: 'It is a tie!' }
+    };
+
+    const outcome = outcomes[humanChoice][computerChoice];
+
+    if (outcome.includes('win')) humanScore += 1;
+    if (outcome.includes('lose')) computerScore += 1;
+
+    console.log(outcome);
+}
