b3e61d821e108ce856ecf0f72e2f58e230cebb77
Alex Wilkom <alex.wilkom@icloud.com>
2024-08-29 13:23:11 +0200

    Create a function that takes in human choice

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+const choices = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber]
+}
+
+function getHumanChoice() {
+    while (true) {
+        const choice = prompt("What do you choose?");
+
+        if (choice === null) {
+            alert("Game cancelled.")
+            return
+        }
+
+        const choiceFormatted = formatString(choice)
+        if (choices.includes(choiceFormatted)) {
+            return choiceFormatted
+        } else {
+            alert("You must choose between rock, paper or scissors.")
+            continue
+        }
+    }
+}
+
+// helper function to capitalize string
+function formatString(str) {
+    return (str[0].toUpperCase() + str.substring(1).toLowerCase());
 }
